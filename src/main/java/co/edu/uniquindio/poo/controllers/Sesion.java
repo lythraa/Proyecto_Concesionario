be7881424e18409897bd37be4c6e154af67d7709
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2024-11-19 12:28:28 -0500

    GestionarTransaccionesController

--- src/main/java/co/edu/uniquindio/poo/controllers/Sesion.java
+++ src/main/java/co/edu/uniquindio/poo/controllers/Sesion.java
@@ -14,4 +14,24 @@ public class Sesion {
     public static Rol getRol() {
         return rolActual;
     }
+
+    private static String id;
+
+    public static Rol getRolActual() {
+        return rolActual;
+    }
+
+    public static void setRolActual(Rol rolActual) {
+        Sesion.rolActual = rolActual;
+    }
+
+    public static String getId() {
+        return id;
+    }
+
+    public static void setId(String id) {
+        Sesion.id = id;
+    }
+
+    
 }

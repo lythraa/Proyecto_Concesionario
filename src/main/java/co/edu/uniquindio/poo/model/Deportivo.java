858083f3aaee4511911dba61a9a0bb62af103614
leidy <lythraa@gmail.com>
2024-11-18 23:50:36 -0500

    update

--- src/main/java/co/edu/uniquindio/poo/model/Deportivo.java
+++ src/main/java/co/edu/uniquindio/poo/model/Deportivo.java
@@ -1,36 +1,24 @@
 package co.edu.uniquindio.poo.model;
 
 public class Deportivo extends Carro {
-    private int numeroPasajeros;
-    private int numeroPuertas;
+        private int numeroPuertas;
     private int numeroBolsasAire;
     private int numeroCaballosDeFuerza;
     private double tiempoParaAlcanzar100KmH;
-
-    
 
     public Deportivo(String matricula, String marca, String modelo, boolean esNuevo, int numeroCambios,
             double velocidadMaxima, String cilindraje, double precio, double precioDia, boolean disponibilidad,
             Transmision transmision, Combustible combustible, int numeroDePasajeros, int númeroDePuertas,
             double capacidadDelMaletero, boolean tieneAireAcondicionado, boolean tieneCámaraDeReversa,
-            int númeroDeBolsasDeAire, boolean tieneABS, int numeroPasajeros, int numeroPuertas, int numeroBolsasAire,
+            int númeroDeBolsasDeAire, boolean tieneABS, int numeroPuertas, int numeroBolsasAire,
             int numeroCaballosDeFuerza, double tiempoParaAlcanzar100KmH) {
         super(matricula, marca, modelo, esNuevo, numeroCambios, velocidadMaxima, cilindraje, precio, precioDia,
                 disponibilidad, transmision, combustible, numeroDePasajeros, númeroDePuertas, capacidadDelMaletero,
                 tieneAireAcondicionado, tieneCámaraDeReversa, númeroDeBolsasDeAire, tieneABS);
-        this.numeroPasajeros = numeroPasajeros;
         this.numeroPuertas = numeroPuertas;
         this.numeroBolsasAire = numeroBolsasAire;
         this.numeroCaballosDeFuerza = numeroCaballosDeFuerza;
         this.tiempoParaAlcanzar100KmH = tiempoParaAlcanzar100KmH;
-    }
-
-    public int getNumeroPasajeros() {
-        return numeroPasajeros;
-    }
-
-    public void setNumeroPasajeros(int numeroPasajeros) {
-        this.numeroPasajeros = numeroPasajeros;
     }
 
     public int getNumeroPuertas() {
